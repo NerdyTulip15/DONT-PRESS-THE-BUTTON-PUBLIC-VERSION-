4468ad4349fa7fcb7c0284a0aa9367db27168b6f
NerdyTulip15 <145475825+NerdyTulip15@users.noreply.github.com>
2024-06-07 10:27:44 -0400

    Update correctkey.py

--- correctkey.py
+++ correctkey.py
@@ -4,7 +4,7 @@ class CorrectKey:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.image = pygame.image.load('key-good.png')
+        self.image = pygame.image.load('key.png')
         self.rescale_image(self.image)
         self.image_size = self.image.get_size()
         self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
